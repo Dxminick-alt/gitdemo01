ca8794aa86d91095ea34c33afffef00dc9234611
Imujin Dominic Munkh-Uils <domdom@Imujins-MacBook-Pro.local>
2025-09-28 16:39:34 +0200

    Initial commit: Hello World program with two methods

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(getGreeting() + " " + getName());
+    }
+
+    // Method 1: To be modified in branchA
+    public static String getGreeting() {
+        return "Hello";
+    }
+
+    // Method 2: To be modified in branchB
+    public static String getName() {
+        return "World";
+    }
+}
